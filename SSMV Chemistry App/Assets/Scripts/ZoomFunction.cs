b1d031f20ffb88610321446c468d199ef92dddf2
mayau654 <59662431+mayau654@users.noreply.github.com>
2020-02-19 14:09:23 -0600

    Added Camera Movement w/ Zoom
    This is not at all how the final zoom function will work, but it is a start.

--- SSMV Chemistry App/Assets/Scripts/ZoomFunction.cs
+++ SSMV Chemistry App/Assets/Scripts/ZoomFunction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ZoomFunction : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 2f;
 
     void Start()
     {
@@ -14,11 +15,32 @@ public class ZoomFunction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // movement code off of internet
 
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
     }
+
+
 
     public void ZoomOnScroll(float value)
     {
         Debug.Log(value);
+
     }
+
 }
