8d4ff28cfc95f2a14715c61070d5be942f2afd34
mayau654 <59662431+mayau654@users.noreply.github.com>
2020-03-04 09:34:10 -0600

    Flipped Zoom Bar and Added Empty Gallery

---
+++ SSMV Chemistry App/Assets/Scripts/MenuToScenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuToScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // opens Gallery when "Gallery" button is hit
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Gallery");
+    }
+}
