384657ed7067f1475123a5dc822493a7fc97e92d
Frank <xuanyang@mail.com>
2020-08-03 20:47:06 -0500

    Instantiation and Clear Button
    -Added instantiation script
    -Added reload level script
    -Added atom selection
    -Added Hydrogen prefab

---
+++ SSMV Chemistry App/Assets/Atoms/Instantiate_Object.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate_Object : MonoBehaviour
+{
+    public GameObject myPrefabObject = null; //set to none
+
+    public void CreateAtom()
+    {
+     Instantiate //creates the object at the set position with no rotation
+            (
+            myPrefabObject,
+            transform.position,
+            Quaternion.identity
+             );   
+    }
+}
---
+++ SSMV Chemistry App/Assets/Scripts/New Scripts/Clear_All.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Scene management
+
+
+public class Clear_All : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //loads the current scene
+    }
+}
