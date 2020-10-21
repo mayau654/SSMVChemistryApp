a31ed4c9bf69c6024728673dc689ebe87eaea33e
Frank <xuanyang@mail.com>
2020-10-20 19:27:39 -0500

    UI Scaling and Bonds 10/20/2020
    -Fixed Scaling problem of the UI
    - Added bond menu and snapping

---
+++ SSMV Chemistry App/Assets/Scripts/Snap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snap : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Atom"))
+        {
+            other.GetComponent<Rigidbody>().MovePosition(transform.position);
+        }
+    }
+}
